a986f4ba42b25cb8b87ae36da33b13f0ff8cecf9
tuanphan1806 <22010282@st.phenikaa-uni.edu.vn>
2026-06-23 08:31:49 +0700

    up

--- code/backend/src/main/java/com/hotel/backend/controller/UserController.java
+++ code/backend/src/main/java/com/hotel/backend/controller/UserController.java
@@ -20,6 +20,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -74,7 +75,7 @@ public class UserController {
     }
 
     @Operation(summary = "Delete User", description = "API delete user to database")
-    @PutMapping("/del/{userId}")
+    @DeleteMapping("/del/{userId}")
     public Map<String,Object> deleteUser(@PathVariable Long userId){
 
         userService.delete(userId);
